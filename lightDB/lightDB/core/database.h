541c8f1e571f5a24c1ea7039066ca86c5d700380
zhengkw18 <49091153+zhengkw18@users.noreply.github.com>
2019-04-12 16:15:12 +0800

    Update database.h

--- lightDB/lightDB/core/database.h
+++ lightDB/lightDB/core/database.h
@@ -3,12 +3,14 @@
 #include <vector>
 #include <map>
 #include "table.h"
+#include "../rename/rename.h"
+#include "../os/serializable.h"
 
 using namespace std;
 /*
     数据库类
 */
-class Database{
+class Database: public Serializable{
     map<string,pTable> tableObjs; //表名-表对象
     vector<string> names;         //按照建立顺序保存表名
 
@@ -19,4 +21,6 @@ class Database{
     void createTable(string tablename, pTable table);
     void dropTable(const string& name);
     pTable getTable(const string& name);
+    string Serialize();
+    static pDatabase Deserialize(const string& content);
 };

6670aaba8fc8e3abbc1ccc3824a67f886e7a2278
zhengkw18 <49091153+zhengkw18@users.noreply.github.com>
2019-04-12 16:00:21 +0800

    Update main.cpp

--- lightDB/lightDB/main.cpp
+++ lightDB/lightDB/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include "core/column.h"
 #include "core/table.h"
-#include "controller/engine.h"
+#include "core/database.h"
+#include "os/s_int.h"
 using namespace std;
 
 int main() {
-	/*Table t;
+	Table t;
 	Column ic(ColumnType::INT, vector<ColumnConstraint>{});
 	ic.add(new Data(3));
 	ic.add(new Data(4));
@@ -28,8 +30,24 @@ int main() {
 	map<string, pData> data1;
 	data1["id"] = new Data(456);
 	data1["name"] = new Data("chenjunhai");
-	t.insert(data1);*/
-	Engine().run();
+	t.insert(data1);
+
+    Database d;
+    d.createTable("t",&t);
+
+    cout<<d.getTable("t")->cols()<<endl;
+
+    string s=d.Serialize();
+    pDatabase pd=Database::Deserialize(s);
+
+    cout<<pd->getTable("t")->getColumn("id")->getData(1)->getIntV()<<endl;
+    cout<<pd->getTable("t")->cols()<<endl;
+    cout<<pd->getTable("t")->getColumn("name")->getData(2)->getCharV()<<endl;
+    cout<<static_cast<int>(pd->getTable("t")->getColumn("name")->getType())<<endl;
+    vector<ColumnConstraint> cons=pd->getTable("t")->getColumn("name")->getConstraints();
+    for(ColumnConstraint con: cons){
+        cout<<"约束"<<static_cast<int>(con)<<endl;
+    }
 	system("pause");
 	return 0;
 }
